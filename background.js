cfaf91dfe20dc3740fe3dd093317337c57ffaecc
Bilal Khan <globin_guy@hotmail.com>
2018-10-18 15:59:31 -0400

    Cleaned up background.js
    Added better documentation, removed unusedcode, changed some hardcoded strings to global variables

--- background.js
+++ background.js
@@ -1,90 +1,93 @@
-var webhooksFile = "webhooks.json"
-var contexts = ["link", "image", "selection", "video", "page"];
+var menus = chrome.contextMenus;
+var storage = chrome.storage.sync;
+const CONTEXTS = ["link", "image", "selection", "video", "page"];
+const NAMESPACES = {
+  webhooks: "webhooks",
+  input: "input"
+};
+const METHOD = "POST"
+const FIELDS = {
+  contents: "content", // I wanted to call this 'content' instead of 'contents' but it literally just didnt work
+  username: "username",
+  avatar_url: "avatar_url"
+};
 
+        // console.log("Status: ", xhr.getResponseHeader("status"));
+
+// Returns the function called when a context menu item is clicked
 function sendReqToWebhookWrap(webhooks) {
+  // info and tab come from onclick method of context menu item
   return function(info, tab) {
-    console.log(info);
-    console.log(info.srcUrl, info.linkUrl, info.selectionText, info.pageUrl);
-    let data;
-    if (info.srcUrl) {
-      data = info.srcUrl;
-    } else if(info.linkUrl) {
-      data = info.linkUrl;
-    } else if(info.selectionText) {
-      data = info.selectionText;
-    } else if(info.pageUrl) {
-      data = info.pageUrl;
-    } else {
-      data = undefined;
-    }
-
-      for (let i = 0; i < webhooks.length; i++) {
-        console.log(webhooks[i]);
-        let url = webhooks[i].url;
-        let username = webhooks[i].username;
-        let avatarUrl = webhooks[i].avatar_url;
-        console.log("url", url);
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onload = () => {
-          console.log("Status: ", xhr.getResponseHeader("status"));
-          console.log("Requests remaining", xhr.getResponseHeader("x-ratelimit-remaining"));
-        }
-        xhr.send(JSON.stringify({"content": data, "username": username, "avatar_url": avatarUrl}));
+    // Determine what to send to webhook
+    // Since a right click can register as multiple different contexts (Ex. right cicking an image can be considered 'image', 'link', or 'page')
+    // Send the first thing in hierarchy that exists: srcUrl, linkUrl, selectionText, pageUrl
+    // If none exist, undefined is sent -> Should never happen, as any click should have a pageUrl, otherwise context menu would not appear
+    let content = info.srcUrl || info.linkUrl || info.selectionText || info.pageUrl;
+    // Iterate through webhooks selected, send XHRs to all
+    for (let i = 0; i < webhooks.length; i++) {
+      console.log("Sending to webhook: " + webhooks[i].title);
+      let url = webhooks[i].url;
+      let username = webhooks[i].username;
+      let avatarUrl = webhooks[i].avatar_url;
+      let xhr = new XMLHttpRequest();
+      xhr.open(METHOD, url, true);
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.onload = () => {
+        console.log("Response Headers");
+        console.log(xhr.getAllResponseHeaders());
       }
+      xhr.send(JSON.stringify({
+        [FIELDS.contents]: content, 
+        [FIELDS.username]: username, 
+        [FIELDS.avatar_url]: avatarUrl
+      }));
+    }
   }
 }
 
 function buildContextMenus(webhooks) {
   console.log(webhooks);
-
-  chrome.contextMenus.removeAll(function(){ console.log("clearing")});
-  let title = "Send to Discord";
-  console.log("Creating menu: parent");
-  chrome.contextMenus.create({
-    "title": title,
-    "contexts": contexts,
+  // Clear out all context menus before readding
+  // Yeah, I'm too lazy to just add new menus when new webhooks are added, so I just clear them all and re-add all
+  menus.removeAll(function() {console.log("clearing")});
+  // Main context menu item
+  menus.create({ 
+    "title": "Send to Discord",
+    "contexts": CONTEXTS,
     "id": "parent"
   });
-  console.log("creating menu: all");
-  chrome.contextMenus.create({
+  // Menu item to send to all webhooks
+  menus.create({
     "title": "Send to all webhooks",
-    "contexts": contexts,
+    "contexts": CONTEXTS,
     "parentId": "parent",
     "id": "all",
     "onclick": sendReqToWebhookWrap(webhooks)
-  })
+  });
+  // Iterating through webhooks, creating context menu for each
   for (let i = 0; i < webhooks.length; i++) {
     console.log("creating menu: " + webhooks[i].url);
-    chrome.contextMenus.create({
+    menus.create({
       "title": "Send to " + webhooks[i].title,
-      "contexts": contexts,
+      "contexts": CONTEXTS,
       "parentId": "parent",
       "id": webhooks[i].url,
       "onclick": sendReqToWebhookWrap([webhooks[i]])
-    })
+    });
   }
 }
 
-function sendCopiedText(info, tab) { }
-
-chrome.storage.sync.get(["webhooks"], function(result) {
+// Grab webhooks from storage on and builds context menus on load
+storage.get([NAMESPACES.webhooks], function(result) {
   if (result.webhooks) {
     buildContextMenus(result.webhooks);
   }
 });
 
+// If changes occur in storage (from popup menu) context menus are rebuilt
 chrome.storage.onChanged.addListener(function(changes, namespace) {
   console.log(changes, namespace);
-  chrome.storage.sync.get(["webhooks"], function(result) {
+  storage.get([NAMESPACES.webhooks], function(result) {
     buildContextMenus(result.webhooks);
   });
 });
-
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     console.log("message received from: ", sender, request.context);
-//     enableContextMenu(request.context);
-//   }
-// );
